6509d5e82dafc18a676ab2ab99fa66e61eca26aa
taitbenj <taitbenj@Ubunru.myguest.virtualbox.org>
2023-10-22 19:30:50 -0400

    push of assgn7p1 code passing fulltest

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -10,8 +10,11 @@
 
 #ifdef __KERNEL__
 #include <linux/string.h>
+#define DEBUG(...) 
 #else
 #include <string.h>
+#include <stdio.h>
+#define DEBUG(...) printf(__VA_ARGS__)
 #endif
 
 #include "aesd-circular-buffer.h"
@@ -29,9 +32,39 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
+    uint8_t cur_buf_size = 0, index = 0;
+    struct aesd_buffer_entry *cur;
+
+    // null pointer checks
+    if(buffer == NULL || entry_offset_byte_rtn == NULL) {
+        return NULL;
+    }
+
+    index = buffer->out_offs;
+    cur = &buffer->entry[index];
+    do {
+        if(char_offset >= cur_buf_size && char_offset < (cur_buf_size + cur->size)) {
+            *entry_offset_byte_rtn = char_offset - cur_buf_size;
+            DEBUG("For global offset %d, found offset %d\n", (int)char_offset, (int)*entry_offset_byte_rtn);
+            DEBUG("Inside of string: %s\n", cur->buffptr);
+            return cur;
+        }
+
+        // Update buffer size
+        if(cur->buffptr != NULL) {
+            cur_buf_size += cur->size;
+        }
+
+        // Increment index and cur
+        index++;
+        if(index >= AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED) {
+            index = 0;
+        }
+        cur = &buffer->entry[index];
+        
+    } while(index != buffer->out_offs);
+
+    // if not found, return null
     return NULL;
 }
 
@@ -44,9 +77,42 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description
-    */
+    // null pointer checks
+    if(buffer == NULL || add_entry == NULL) {
+        DEBUG("Error: null pointer passed!\n");
+        return;
+    }
+
+    // Add new entry
+    buffer->entry[buffer->in_offs].size = add_entry->size;
+    buffer->entry[buffer->in_offs].buffptr = add_entry->buffptr;
+
+    // Move up in_offs
+    if(buffer->in_offs + 1 >= AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED) {
+        buffer->in_offs = 0;
+    } else {
+        buffer->in_offs++;
+    }
+
+    // Check for buffer full
+    if(buffer->in_offs == buffer->out_offs || buffer->full == true) {
+        buffer->full = true;
+
+        // Move up out_offs
+        buffer->out_offs = buffer->in_offs;
+    }
+
+
+    DEBUG("buffer: {\n");
+    for(int i=0; i<10; i++) {
+        if(buffer->entry[i].buffptr != NULL) {
+            DEBUG("%c%c[%d] %s", (i==buffer->in_offs)?'i':' ', \
+                                (i==buffer->out_offs)?'o':' ', \
+                                i, buffer->entry[i].buffptr);
+        }
+    }
+    DEBUG("}\n");
+
 }
 
 /**
